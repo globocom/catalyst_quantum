71d019dabfda65f708a1744227d0dba965370e79
Flavia Missi <flaviamissi@gmail.com>
2012-08-22 11:42:03 -0300

    databse configs

---
+++ common/config.py
@@ -0,0 +1,11 @@
+from quantum.openstack.common import cfg
+
+
+database_opts = [
+    cfg.StrOpt('sql_connection', default='sqlite://'),
+                  cfg.IntOpt('sql_max_retries', default=-1),
+                                cfg.IntOpt('reconnect_interval', default=2),
+]
+
+
+cfg.CONF.register_opts(database_opts, "DATABASE")
