adf0fb8adf993c0dbb0ac96ced6a70562cb9b66a
Flavia Missi <flaviamissi@gmail.com>
2012-08-22 11:55:54 -0300

    created base for plugin tests

---
+++ tests/unit/test_plugin.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+
+class SubnetTestCase(TestCase):
+
+    def test_create_subnet(self):
+        pass
+
+    def test_create_subnet_with_unsuficient_params(self):
+        pass
+
+    def test_update_subnet(self):
+        pass
+
+    def test_delete_subnet(self):
+        pass
+
+    def test_get_subnet(self):
+        pass
+
+    def test_get_subnets(self):
+        pass
+
+
+class NetworkTestCase(TestCase):
+
+    def test_create_network(self):
+        pass
+
+    def test_update_network(self):
+        pass
+
+    def test_delete_network(self):
+        pass
+
+    def test_get_network(self):
+        pass
+
+    def test_get_networks(self):
+        pass
+
+
+class PortTestCase(TestCase):
+
+    def test_create_port(self):
+        pass
+
+    def test_update_port(self):
+        pass
+
+    def test_delete_port(self):
+        pass
+
+    def test_get_port(self):
+        pass
+
+    def test_get_ports(self):
+        pass
