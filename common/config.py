008f6ddc860483d8be44de7a76f57e8df6d0ac6e
Flavia Missi <flaviamissi@gmail.com>
2012-08-22 11:46:04 -0300

    fixing indentation

--- common/config.py
+++ common/config.py
@@ -3,8 +3,8 @@ from quantum.openstack.common import cfg
 
 database_opts = [
     cfg.StrOpt('sql_connection', default='sqlite://'),
-                  cfg.IntOpt('sql_max_retries', default=-1),
-                                cfg.IntOpt('reconnect_interval', default=2),
+    cfg.IntOpt('sql_max_retries', default=-1),
+    cfg.IntOpt('reconnect_interval', default=2),
 ]
 
 
